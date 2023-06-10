7548ec1e51f19e2352e5e9debdda67d39c98ac76
Bion Howard <flesheatingemu@gmail.com>
2023-06-10 15:56:15 -0400

    attempt to handle unicode issue with emojis on windows

--- tree_plus_cli.py
+++ tree_plus_cli.py
@@ -1,6 +1,8 @@
 # src/cli.py
 from typing import Optional, Union
 from collections import defaultdict
+import platform
+import sys
 import os
 import re
 
@@ -42,6 +44,16 @@ def main(directories, ignore, color):
 
 def tree_plus(directory: str, ignore: Optional[Union[str, set]] = None) -> Tree:
     "an enhanced tree util with file component leaves and token / line counts"
+
+    # Check for platform and encoding
+    use_special_chars = not (
+        platform.system() == "Windows" and sys.stdout.encoding == "cp1252"
+    )
+
+    # Use special characters or alternatives based on the check
+    folder_char = "\U0001f4c1" if use_special_chars else ":file_folder:"
+    file_char = "\U0001f4c4" if use_special_chars else ":page_facing_up:"
+
     # QUESTION: Could this screw up the parent_dir_path in the path_to_tree dict?
     directory = os.path.abspath(directory)
 
@@ -104,7 +116,7 @@ def tree_plus(directory: str, ignore: Optional[Union[str, set]] = None) -> Tree:
         for part in path_parts:
             parent_dir_path = os.path.dirname(part)
             parent_tree = path_to_tree[parent_dir_path]
-            dir_tree = parent_tree.add(f":file_folder: {os.path.basename(part)}")
+            dir_tree = parent_tree.add(f"{folder_char} {os.path.basename(part)}")
             path_to_tree[part] = dir_tree
 
     # Here, we sort directories like Linux tree utility
@@ -122,19 +134,19 @@ def tree_plus(directory: str, ignore: Optional[Union[str, set]] = None) -> Tree:
             file_count = count_tokens_lines(file_path)
             dir_count.n_tokens += file_count.n_tokens
             dir_count.n_lines += file_count.n_lines
-            file_label = f":page_facing_up: {file_name}"
+            file_label = f"{file_char} {file_name}"
             file_label += f" ({file_count.n_tokens} tokens, {file_count.n_lines} lines)"
             file_tree = dir_tree.add(file_label)
             for component in components:
                 file_tree.add(component)
 
-        dir_label = f":file_folder: {os.path.basename(dir_path)}"
+        dir_label = f"{folder_char} {os.path.basename(dir_path)}"
         dir_label += f" ({dir_count.n_tokens} tokens, {dir_count.n_lines} lines)"
         dir_tree.label = dir_label
         total_count.n_tokens += dir_count.n_tokens
         total_count.n_lines += dir_count.n_lines
 
-    root_tree.label = f":file_folder: {os.path.basename(os.path.abspath(directory))} ({total_count.n_tokens} tokens, {total_count.n_lines} lines)"
+    root_tree.label = f"{folder_char} {os.path.basename(os.path.abspath(directory))} ({total_count.n_tokens} tokens, {total_count.n_lines} lines)"
     return root_tree
 
 
@@ -149,147 +161,3 @@ def tree_to_string(tree: Tree) -> str:
 
 if __name__ == "__main__":
     main()
-
-
-# def tree_plus(directory: str, ignore: Optional[Union[str, set]] = None) -> Tree:
-#     "?"
-#     # Resolve the absolute path from the start
-#     directory = os.path.abspath(directory)
-
-#     # Handle the ignore argument and unify it into a set
-#     if ignore is None:
-#         ignore = DEFAULT_IGNORE
-#     elif isinstance(ignore, str):
-#         ignore = set(ignore.split(","))
-#         ignore |= DEFAULT_IGNORE
-#     elif isinstance(ignore, set):
-#         ignore |= DEFAULT_IGNORE
-#     else:
-#         raise TypeError("tree_plus ignore arg must be a string, set or None")
-
-#     root_tree = Tree(
-#         f"{directory} ({0} tokens, {0} lines)",
-#         guide_style="bold cyan",
-#         highlight=True,
-#     )
-
-#     file_paths = traverse_directory(directory, ignore)
-#     total_count = TokenLineCount(n_tokens=0, n_lines=0)
-
-#     # Group files by their directories
-#     directories = {}
-#     for file_path in file_paths:
-#         dir_path, file_name = os.path.split(file_path)
-#         if dir_path not in directories:
-#             directories[dir_path] = []
-#         directories[dir_path].append(file_name)
-
-#     print(f"Directories after grouping files: {directories}")  # Debug print
-#     # At this point, directories should be a dict mapping paths to files
-
-#     # Now, we'll construct the tree...
-#     for dir_path, files in directories.items():
-#         print(f"Processing directory: {dir_path}")  # Debug print
-#         basename = os.path.basename(os.path.abspath(dir_path))
-#         parent_tree = root_tree  # by default, the parent is the root
-
-#         # If the directory is not the root directory, find its parent
-#         if basename and basename != os.path.basename(directory):
-#             parent_tree = find_parent_tree(root_tree, basename)
-
-#         dir_count = TokenLineCount(n_tokens=0, n_lines=0)
-#         for file_name in files:
-#             file_path = os.path.join(dir_path, file_name)
-#             components = parse_file(file_path)
-#             file_count = count_tokens_lines(file_path)
-#             dir_count.n_tokens += file_count.n_tokens
-#             dir_count.n_lines += file_count.n_lines
-#             file_label = f":page_facing_up: {file_name}"
-#             file_label += f" ({file_count.n_tokens} tokens, {file_count.n_lines} lines)"
-#             file_tree = parent_tree.add(file_label)
-#             for component in components:
-#                 file_tree.add(component)
-
-#         if basename and basename != os.path.basename(directory):
-#             parent_tree.label += (
-#                 f" ({dir_count.n_tokens} tokens, {dir_count.n_lines} lines)"
-#             )
-
-#         total_count.n_tokens += dir_count.n_tokens
-#         total_count.n_lines += dir_count.n_lines
-
-#     root_tree.label = f"{os.path.abspath(directory)} ({total_count.n_tokens} tokens, {total_count.n_lines} lines)"
-#     return root_tree
-
-
-# def find_parent_tree(root_tree, basename):
-#     # Function to find the parent tree of a given directory
-#     for child in root_tree.children:
-#         if basename in child.label:
-#             return child
-#     return root_tree
-
-
-# def tree_plus(directory: str, ignore: Optional[Union[str, set]] = None) -> Tree:
-#     "?"
-#     # Resolve the absolute path from the start
-#     directory = os.path.abspath(directory)
-#     if ignore is None:
-#         ignore = DEFAULT_IGNORE
-#     elif isinstance(ignore, str):
-#         ignore = set(ignore.split(","))
-#         ignore |= DEFAULT_IGNORE
-#     elif isinstance(ignore, set):
-#         ignore |= DEFAULT_IGNORE
-#     else:
-#         raise TypeError("tree_plus ignore arg must be a string, set or None")
-
-#     root_tree = Tree(
-#         f"{directory} ({0} tokens, {0} lines)",
-#         guide_style="bold cyan",
-#         highlight=True,
-#     )
-
-#     file_paths = traverse_directory(directory, ignore)
-#     total_count = TokenLineCount(n_tokens=0, n_lines=0)
-
-#     # Group files by their directories
-#     directories = {}
-#     for file_path in file_paths:
-#         dir_path, file_name = os.path.split(file_path)
-#         if dir_path not in directories:
-#             directories[dir_path] = []
-#         directories[dir_path].append(file_name)
-
-#     print(f"Directories after grouping files: {directories}")  # Debug print
-#     # At this point, directories should be a dict mapping paths to files
-#     # Now, we'll construct the tree...
-#     for dir_path, files in directories.items():
-#         print(f"Processing directory: {dir_path}")  # Debug print
-#         basename = os.path.basename(os.path.abspath(dir_path))
-#         # If basename is not an empty string, add it as a directory to the tree.
-#         # Here, we use the original dir_path value to keep ".." as it is, if that's the case.
-#         if basename and basename != os.path.basename(directory):
-#             dir_tree = root_tree.add(f":file_folder: {os.path.basename(dir_path)}")
-#             dir_count = TokenLineCount(n_tokens=0, n_lines=0)
-#             for file_name in files:
-#                 file_path = os.path.join(dir_path, file_name)
-#                 components = parse_file(file_path)
-#                 file_count = count_tokens_lines(file_path)
-#                 dir_count.n_tokens += file_count.n_tokens
-#                 dir_count.n_lines += file_count.n_lines
-#                 file_label = f":page_facing_up: {file_name}"
-#                 file_label += (
-#                     f" ({file_count.n_tokens} tokens, {file_count.n_lines} lines)"
-#                 )
-#                 file_tree = dir_tree.add(file_label)
-#                 for component in components:
-#                     file_tree.add(component)
-#             dir_label = f":file_folder: {os.path.basename(dir_path)}"
-#             dir_label += f" ({dir_count.n_tokens} tokens, {dir_count.n_lines} lines)"
-#             dir_tree.label = dir_label
-#             total_count.n_tokens += dir_count.n_tokens
-#             total_count.n_lines += dir_count.n_lines
-
-#     root_tree.label = f"{os.path.abspath(directory)} ({total_count.n_tokens} tokens, {total_count.n_lines} lines)"
-#     return root_tree
