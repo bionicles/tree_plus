06402d0e88490f3027dbc1ca2eac14f7111b0128
Bion Howard <flesheatingemu@gmail.com>
2023-12-28 22:03:05 -0500

    fix: no cache debug

--- tree_plus_src/debug.py
+++ tree_plus_src/debug.py
@@ -1,6 +1,5 @@
 # tree_plus_src/debug.py
 import os
-from functools import cache
 from rich import print
 
 
@@ -8,7 +7,6 @@ def disable_debug():
     os.environ["DEBUG_TREE_PLUS"] = "0"
 
 
-@cache
 def debug_enabled():
     return os.environ.get("DEBUG_TREE_PLUS") == "1"
 
@@ -20,4 +18,3 @@ def debug_print(*args, **kwargs):
 
 def enable_debug():
     os.environ["DEBUG_TREE_PLUS"] = "1"
-    debug_enabled.cache_clear()
