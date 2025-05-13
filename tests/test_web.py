cc1ab82560fbd9d05323951d59ae05c4832e468f
Bion Howard <flesheatingemu@gmail.com>
2025-05-13 19:50:12 -0400

    fix(test_web): reduce the amount of getting from hacker news in the web test

--- tests/test_web.py
+++ tests/test_web.py
@@ -2,5 +2,5 @@ from tree_plus_src import web
 
 
 def test_articles_from_hacker_news():
-    articles = web.articles_from_hacker_news()
+    articles = web.articles_from_hacker_news(max_depth=1, n_articles=1)
     assert bool(articles)
